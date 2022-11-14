9cba0bd9a3810c927903a06bd0afc0fe4cd051fe
lwincho-dev <lwinlwincho135324@gmail.com>
2022-11-14 05:55:45 -0800

    chore: remove uncessary code.

--- app/src/main/java/com/llc/moviebd/database/MovieRoomDatabase.kt
+++ app/src/main/java/com/llc/moviebd/database/MovieRoomDatabase.kt
@@ -1,31 +1,9 @@
 package com.llc.moviebd.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [FavouriteMovieEntity::class], version = 2)
 abstract class MovieRoomDatabase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: MovieRoomDatabase? = null
-
-        fun getDatabase(context: Context): MovieRoomDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context,
-                    MovieRoomDatabase::class.java,
-                    "movie_database"
-                )
-                    .allowMainThreadQueries()
-                    .fallbackToDestructiveMigration()
-                    .build()
-                INSTANCE = instance
-                return instance
-            }
-        }
-    }
 }
